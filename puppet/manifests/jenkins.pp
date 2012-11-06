0242f3f64fd344a01c32029812eafc6d0cf2b13b
Joao Costa <joaocosta@zonalivre.org>
2012-11-06 23:23:33 +0000

    Fix package name

--- puppet/manifests/jenkins.pp
+++ puppet/manifests/jenkins.pp
@@ -1,9 +1,9 @@
 include jenkins
 
-package { "java":
+package { "java-1.7.0-openjdk":
     ensure  => latest,
 }
 
 jenkins::plugin { "git":
-    require =>  [ Class["jenkins::package"], Package["java"] ],
+    require =>  [ Class["jenkins::package"], Package["java-1.7.0-openjdk"] ],
 }
