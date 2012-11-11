d0ede05752d267a8ab858fedc7f08b6eb14027b0
João Costa <joaocosta@zonalivre.org>
2012-11-11 03:56:42 +0000

    Install mach to be able to build rpms in chroot environments

--- puppet/modules/buildbox/manifests/params.pp
+++ puppet/modules/buildbox/manifests/params.pp
@@ -1,7 +1,7 @@
 class buildbox::params {
 
     if $::osfamily == 'redhat' or $::operatingsystem == 'amazon' {
-        $build_packages     = ['gcc', 'make', 'cpanspec']
+        $build_packages     = ['gcc', 'make', 'cpanspec', 'mach']
     } elsif $::osfamily == 'debian' {
         $build_packages     = ['build-essential', 'dh-make-perl']
     } else {
