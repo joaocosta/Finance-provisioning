27ac81fe4260ef0d95a50eb235e72b5fa5b3f63d
João Costa <joaocosta@zonalivre.org>
2012-11-11 07:21:53 +0000

    Make sure user has permission to use mach

--- puppet/modules/buildbox/manifests/init.pp
+++ puppet/modules/buildbox/manifests/init.pp
@@ -1,6 +1,7 @@
 class buildbox {
-
     include buildbox::params
+
+    Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 
     package { "ruby-devel":
         ensure      => latest,
@@ -15,4 +16,14 @@ class buildbox {
     package { $buildbox::params::build_packages:
         ensure      => latest,
     }
+
+    include users
+    realize Users::Mkuser["joao"]
+
+    exec { "allow joao":
+        command => "usermod -a -G mach joao",
+        require => [Package["mach"], User["joao"]],
+    }
+
 }
+
