0a193e0b231ec7ac507e6b3481c4ab113d1ff0bc
João Costa <joaocosta@zonalivre.org>
2012-11-13 00:02:48 +0000

    Install Dist::Zilla in the build box

--- puppet/modules/buildbox/manifests/params.pp
+++ puppet/modules/buildbox/manifests/params.pp
@@ -1,9 +1,9 @@
 class buildbox::params {
 
     if $::osfamily == 'redhat' or $::operatingsystem == 'amazon' {
-        $build_packages     = ['gcc', 'gcc-c++', 'make', 'cpanspec', 'mach']
+        $build_packages     = ['gcc', 'gcc-c++', 'make', 'perl-Dist-Zilla', 'mach', 'cpanspec']
     } elsif $::osfamily == 'debian' {
-        $build_packages     = ['build-essential', 'dh-make-perl']
+        $build_packages     = ['build-essential', 'libdist-zilla-perl', 'dh-make-perl']
     } else {
         fail("Class['buildbox::params']: Unsupported operatingsystem: $operatingsystem")
     }
