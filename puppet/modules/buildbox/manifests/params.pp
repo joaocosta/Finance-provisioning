c7e7c0a5b9cb78b28ffa571317e345e8efef4442
João Costa <joaocosta@zonalivre.org>
2012-11-13 00:16:56 +0000

    Install App::cpanminus

--- puppet/modules/buildbox/manifests/params.pp
+++ puppet/modules/buildbox/manifests/params.pp
@@ -2,10 +2,10 @@ class buildbox::params {
 
     if $::osfamily == 'redhat' or $::operatingsystem == 'amazon' {
         $build_packages     = ['gcc', 'gcc-c++', 'make', 'mach', 'cpanspec']
-        $perl_build         = ['perl-Dist-Zilla']
+        $perl_build         = ['perl-Dist-Zilla', 'perl-App-cpanminus']
     } elsif $::osfamily == 'debian' {
         $build_packages     = ['build-essential', 'dh-make-perl']
-        $perl_build         = ['libdist-zilla-perl']
+        $perl_build         = ['libdist-zilla-perl', 'libapp-cpanminus-perl']
     } else {
         fail("Class['buildbox::params']: Unsupported operatingsystem: $operatingsystem")
     }
