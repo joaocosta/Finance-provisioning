24ac4e4cd5ddab4e54ea76eafaff698279398e8b
João Costa <joaocosta@zonalivre.org>
2012-11-23 21:54:03 +0000

    Need zonalivre repo to build certain packages

--- puppet/modules/buildbox/manifests/params.pp
+++ puppet/modules/buildbox/manifests/params.pp
@@ -4,6 +4,13 @@ class buildbox::params {
         $build_packages     = ['gcc', 'gcc-c++', 'make', 'mach', 'cpanspec']
         $perl_build         = ['perl-Dist-Zilla', 'perl-Pod-Elemental-Transformer-List', 'perl-App-cpanminus']
         $perl_utils         = ['perl-Pod-Perldoc']
+
+        file { "/var/lib/mach/states/fedora-16-x86_64-updates/yum/yum.repos.d/zonalivre.repo":
+            ensure  => present,
+            source  => 'puppet:///modules/zonalivre_repo/zonalivre.repo',
+            group   => 'mach',
+            mode    => '0644',
+        }
     } elsif $::osfamily == 'debian' {
         $build_packages     = ['build-essential', 'dh-make-perl']
         $perl_build         = ['libdist-zilla-perl', 'libapp-cpanminus-perl']
