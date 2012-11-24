6d0a170ea62b2c25a788709df48a658c7e593fc6
João Costa <joaocosta@zonalivre.org>
2012-11-24 17:43:42 +0000

    Fix permissions/dependencies

--- puppet/modules/buildbox/manifests/params.pp
+++ puppet/modules/buildbox/manifests/params.pp
@@ -5,11 +5,33 @@ class buildbox::params {
         $perl_build         = ['perl-Dist-Zilla', 'perl-Pod-Elemental-Transformer-List', 'perl-App-cpanminus']
         $perl_utils         = ['perl-Pod-Perldoc']
 
+        file { "/var/lib/mach/states/fedora-16-x86_64-updates":
+            ensure  => directory,
+            group   => 'mach',
+            mode    => '0775',
+            require => Package['mach'],
+        }
+
+        file { "/var/lib/mach/states/fedora-16-x86_64-updates/yum":
+            ensure  => directory,
+            group   => 'mach',
+            mode    => '0775',
+            require => File["/var/lib/mach/states/fedora-16-x86_64-updates"],
+        }
+
+        file { "/var/lib/mach/states/fedora-16-x86_64-updates/yum/yum.repos.d":
+            ensure  => directory,
+            group   => 'mach',
+            mode    => '0775',
+            require => File["/var/lib/mach/states/fedora-16-x86_64-updates/yum"],
+        }
+
         file { "/var/lib/mach/states/fedora-16-x86_64-updates/yum/yum.repos.d/zonalivre.repo":
             ensure  => present,
             source  => 'puppet:///modules/zonalivre_repo/zonalivre.repo',
             group   => 'mach',
             mode    => '0644',
+            require => File["/var/lib/mach/states/fedora-16-x86_64-updates/yum/yum.repos.d"],
         }
     } elsif $::osfamily == 'debian' {
         $build_packages     = ['build-essential', 'dh-make-perl']
