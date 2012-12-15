82ff1c1573951f0d896b17454ae159f0e0d99909
João Costa <joaocosta@zonalivre.org>
2012-12-15 01:17:50 +0000

    Puppet manifest that installs everything

---
+++ puppet/manifests/site.pp
@@ -0,0 +1,59 @@
+Exec { path => [ '/usr/bin' ] }
+
+stage { 'init':
+    before  => Stage['main'],
+}
+
+include users
+realize Users::Mkuser['joao']
+
+#exec { 'mount_src':
+#    command => 'mount -t vboxsf -o uid=`id -u joao`,gid=`id -g joao` v-data /opt/src',
+#    require => [ User['joao'], Group['joao'] ],
+#}
+
+#mount { 'opt/src':
+#    atboot  => 'false',
+#    device  => 'v-data',
+#    ensure  => 'mounted',
+#    fstype  => 'vboxsf',
+#    remounts=> 'true',
+#    options => 'defaults',
+#}
+
+file { '/etc/localtime':
+    ensure  => link,
+    target  => '/usr/share/zoneinfo/UTC',
+    stage   => 'init',
+}
+
+class { 'zonalivre_repo::client':
+    stage   => 'init',
+}
+
+package { 'selinux-policy-targeted':
+    ensure  => absent,
+}
+
+package { 'selinux-policy':
+    ensure  => absent,
+    require => Package['selinux-policy-targeted'],
+}
+
+class { 'zonalivre_repo::server':
+    base_path   => '/home/joao/rpmbuild',
+    user        => 'joao',
+    group       => 'joao',
+}
+
+class { 'fxtrader': }
+class { 'fxtrader::test': }
+class { 'fxtrader::website': }
+
+class { 'buildbox':
+    user => 'joao',
+}
+
+class { 'site':
+    user    => 'joao',
+}
