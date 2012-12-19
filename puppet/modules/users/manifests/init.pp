6cbf569a0d2089bf1e565f4cecc1dbf722e7d06d
João Costa <joaocosta@zonalivre.org>
2012-12-19 00:24:24 +0000

    Reorganize code for readability

--- puppet/modules/users/manifests/init.pp
+++ puppet/modules/users/manifests/init.pp
@@ -15,18 +15,39 @@
 # [Remember: No empty lines between comments and class definition]
 class users {
 
-    define sudonopasswd( ) {
-        augeas { "sudo${name}":
-            context => "/files/etc/sudoers",
-            changes => [
-                "set spec[user = '${name}']/user ${name}",
-                "set spec[user = '${name}']/host_group/host ALL",
-                "set spec[user = '${name}']/host_group/command ALL",
-                "set spec[user = '${name}']/host_group/command/runas_user ALL",
-                "set spec[user = '${name}']/host_group/command/tag NOPASSWD",
-            ],
-        }
-    }
+    # this is all listed here and realized within the module with 'realize Generic::Mkuser[username]'
+    # it is here and not in the modules, so that we have one place to list all the uid/gid's
+    # to avoid using the same numbers
+    #
+    # please keep sorted by UID
+    @mkuser {
+        "joao":
+            uid        => "40001",
+            gid        => "40001",
+            managehome => "true",
+            comment    => "Joao Costa",
+            authorized_key_ensure => present,
+            authorized_key_type   => "ssh-rsa",
+            authorized_key_string => "AAAAB3NzaC1yc2EAAAADAQABAAABAQC6hE75Ox6wDfXVJzXeKdyUBO4o19TtGxboJTI2vR3CE9ZJbODIxSr+tfMZcwmuSF892PiahhVzAA2wJ6LdMtFH6FUIGvjU0i7jIo/x+TmvheH46N9qllo2C2ZlxL/HbpRYIyqEntUYcBQzYBvUwnzoDFgS1GhG4LalYp0U9zlHGOA/Wk7qBjH8Ca1mtPSnxudsb/NwERIjfLbvdX9Fc+vkx6fs3ykJv+p8lPEZkw3kcVAfuyhnXzE7kprSHDuOuQo0FDvCTjy9ISxZPvExKT7bD7vQRlrx9PLzYSWI7/evonWHR8c/jPS8U56ii8YH/rtC/iqo4LiwKFxoxaDdS2wD",
+            sudonopasswd => true,
+            ;
+    } # @mkuser
+
+    # this is all listed here and realized within the module with 'realize Generic::Mkgroup[groupname]'
+    # it is here and not in the modules, so that we have one place to list all the uid/gid's
+    # to avoid using the same numbers
+    #
+    # please keep sorted by GID
+#    @mkgroup {
+#        "dev":
+#            gid => "30002";
+#    } # @mkgroup
+
+
+    # which of the users above should be absent
+    $users_to_remove = []
+    rmuser { $users_to_remove: }
+
 
     # This definition is a convenience wrapper around 
     # Puppet's existing user/goup/authorized_key resources.
@@ -137,28 +158,6 @@ class users {
     } # define mkgroup
 
 
-    $users_to_remove = []
-
-    # this is all listed here and realized within the module with 'realize Generic::Mkuser[username]'
-    # it is here and not in the modules, so that we have one place to list all the uid/gid's
-    # to avoid using the same numbers
-    #
-    # please keep sorted by UID
-    @mkuser {
- 	"joao":
-            uid        => "40001",
-            gid        => "40001",
-            managehome => "true",
-            comment    => "Joao Costa",
-            authorized_key_ensure => present,
-            authorized_key_type   => "ssh-rsa",
-            authorized_key_string => "AAAAB3NzaC1yc2EAAAADAQABAAABAQC6hE75Ox6wDfXVJzXeKdyUBO4o19TtGxboJTI2vR3CE9ZJbODIxSr+tfMZcwmuSF892PiahhVzAA2wJ6LdMtFH6FUIGvjU0i7jIo/x+TmvheH46N9qllo2C2ZlxL/HbpRYIyqEntUYcBQzYBvUwnzoDFgS1GhG4LalYp0U9zlHGOA/Wk7qBjH8Ca1mtPSnxudsb/NwERIjfLbvdX9Fc+vkx6fs3ykJv+p8lPEZkw3kcVAfuyhnXzE7kprSHDuOuQo0FDvCTjy9ISxZPvExKT7bD7vQRlrx9PLzYSWI7/evonWHR8c/jPS8U56ii8YH/rtC/iqo4LiwKFxoxaDdS2wD",
-            sudonopasswd => true,
-            ;
-       } # @mkuser
-
-    rmuser { $users_to_remove: }
-
     define rmuser (
     ) {
         user { "$name":
@@ -184,16 +183,17 @@ class users {
         }
     } # define rmuser
 
-
-
-    # this is all listed here and realized within the module with 'realize Generic::Mkgroup[groupname]'
-    # it is here and not in the modules, so that we have one place to list all the uid/gid's
-    # to avoid using the same numbers
-    #
-    # please keep sorted by GID
-    @mkgroup {
-        "dev":
-            gid => "30002";
-    } # @mkgroup
+    define sudonopasswd( ) {
+        augeas { "sudo${name}":
+            context => "/files/etc/sudoers",
+            changes => [
+                "set spec[user = '${name}']/user ${name}",
+                "set spec[user = '${name}']/host_group/host ALL",
+                "set spec[user = '${name}']/host_group/command ALL",
+                "set spec[user = '${name}']/host_group/command/runas_user ALL",
+                "set spec[user = '${name}']/host_group/command/tag NOPASSWD",
+            ],
+        }
+    }
 
 } # class generic
