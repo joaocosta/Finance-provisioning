3f0cec9a977987ca568295754295241363ab2588
João Costa <joaocosta@zonalivre.org>
2014-01-03 01:49:18 +0000

    Updated to work with latest version of puppet mysql

--- puppet/modules/fxtrader/manifests/init.pp
+++ puppet/modules/fxtrader/manifests/init.pp
@@ -7,12 +7,14 @@ Exec {
 class { 'mysql::server':
 }
 
+class { 'mysql::client':
+}
+
 mysql::db { 'fxcm':
     user     => 'fxcm',
     password => 'fxcm',
     host     => 'localhost',
     grant    => ['all'],
-    require  => File['/root/.my.cnf'], #This resource is declared in the mysql module. There's a bug there because it tries to use "/root/.my.cnf" before it's been deployed, so i force the dependency here to workaround that. Raised http://projects.puppetlabs.com/issues/17802, someone there proposed a different workaround using stages. Note that just requiring Class['mysql::server'] is not enough to work around this.
 }
 
 mysql::db { 'fx':
@@ -20,7 +22,6 @@ mysql::db { 'fx':
     password => 'fxhistor',
     host     => 'localhost',
     grant    => ['all'],
-    require  => File['/root/.my.cnf'],
 }
 
 exec {"fetch_db_schema":
@@ -32,7 +33,7 @@ exec {"fetch_db_schema":
 
 exec { "setup_db_tables":
     unless      => "/usr/bin/mysql -ufxcm -pfxcm -e 'select count(1) from EURUSD_86400' fxcm",
-    require     => [Exec["fetch_db_schema"], Database['fxcm']],
+    require     => [Exec["fetch_db_schema"], Mysql_database['fxcm']],
     command     => "/usr/bin/mysql -uroot fxcm < /tmp/db_schema.sql",
     logoutput   => "on_failure",
 }
@@ -51,7 +52,7 @@ define setup_libmysqludf_ta {
     exec { "setup libmysqludf_ta_${name}":
         command => "mysql -uroot < /usr/share/libmysqludf_ta/lib_mysqludf_ta_${name}_up",
         unless  => "test $(mysql -N -uroot -e 'select count(1) from mysql.func where name = \"ta_${name}\"') -eq 1",
-        require => [ Package['libmysqludf_ta'], Class['mysql::server'], Class['mysql'] ],
+        require => [ Package['libmysqludf_ta'], Class['mysql::server'], Class['mysql::client'] ],
     }
 }
 
